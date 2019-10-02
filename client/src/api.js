28ca2a7058f430f506c44893cdde5365abfbf0e9
Doublelayer <florin.hamann@web.de>
2019-10-02 21:39:29 +0200

    update dev api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  window.location.hostname === "localhost" ? "http://localhost:5000/api/v1/" : "https://twitter-clone-api.doublelayer.now.sh/";
+  window.location.hostname === "localhost" ? "http://localhost:5000/api/v1" : "https://twitter-clone-api.doublelayer.now.sh/api/v1";
 
 export default {
   post: `${API_BASE_URL}/post/tweet`,
